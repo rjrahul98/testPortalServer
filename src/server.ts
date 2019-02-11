5fd74655b1d3c40209275f30eba657ba6ce19f64
rjrahul98 <rjrahul1711@gmail.com>
2019-02-11 14:22:13 +0530

    no message

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+import {Db} from './DB'
+import { configRoutes } from 'routes';
+
+
+export const app = express();
+const dbClient = new Db();
+dbClient.connectMongooseServer();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+configRoutes(app);
+
+let port : number = 4000;
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`);
+})
